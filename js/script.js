8a75958d5131a3708492556da07c4d8da69032d5
Rumki khatum <rumkikhantum@gmail.com>
2022-03-28 11:18:53 +0600

    updated virsion 0.1

---
+++ js/script.js
@@ -0,0 +1,49 @@
+
+
+/*
+    == git commmand ==
+    int 'for initialize'
+    git config --global user.name 'user name'
+    git config --global user.eamil 'user email address'
+    git config --list
+    git commit-m 'measn updated and final stage "m means message "'
+    git log 'for showing updated virsion number'
+    git log --oneline 'it will readuce updated virsion number's number or code'
+    git show updated number 'it will show you your all the updated'
+    git commit-am 'it will avoid staging area it will just commit'
+    wq and Enter press 'for avoid end '
+    git checkout 'for change brach'
+    git switch -c nameOfBranch 'you can create a new branch'
+    git switch 'go back in previous branch'
+    git add 
+    git add .
+*/
+
+/* 
+-------------------------------------------
+ gitHub class 2 start here
+ 
+ git diff 'for logcal stage'
+ git diff --staged 'for staged repojitory'
+ git revert 'mean's undo it means we are just commit now but we think we need to change something of it'
+ git commit --amend 'if we commit unfortunatly then we can modify and add those file then our file will be updated but update number won't be change'
+-------------------------------------- -*practice*-revert updated
+*/
+// git commit --amend start here
+/*
+    const test = document.querySelector('.test);
+    console.log('I love my this is world do you know it')
+*/
+
+// git and gitHub part - 3  js
+/*
+    .gitignore 'for any file igoner this folder'
+    git branch name 'for crating new branch'
+    git merge comment 'locally merge'
+
+    git push -u origin 'github pushing'
+    git clone https/ 'code path nia astea hobay'
+    git pull enter 'one's project but working many people those time if we want to tader updated change ane thar pur amer ta hoby tahole git pull ant then git push -u origin thita hobay'
+    git newBranch -- not main
+
+*/
